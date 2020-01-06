82543b3944cf144a15e1095ebef61c1718ca07b5
John Hunt <john@matraining.com>
2020-01-06 12:16:26 +0000

    Added mongodb functionality

--- lab9-loading-data-from-mongodb/models/books.js
+++ lab9-loading-data-from-mongodb/models/books.js
@@ -6,6 +6,11 @@ const DATABASE_NAME = "bookshopdb";
 const COLLECTION_NAME = "books";
 var collection;
 
+/*
+Function sets up the mongodb database connection
+and obtains a reference to the collection which is used throughout
+this example
+*/
 function setupMongo() {
   MongoClient.connect(
     URL,
@@ -35,33 +40,55 @@ class BookError extends Error {
 }
 
 // Define models functions
+// Now these have bene updated to take a response object
+// so that the callback and provide a response back to the client when
+// it runs - avoids use of awaits etc.
 
-function getAllBooks() {
-  var books;
-  collection.find({}).toArray((err, results) => {
+function getAllBooks(res) {
+  console.log("model.getAllBooks()");
+  return collection.find().toArray((err, books) => {
+    console.log("Books found: ", books);
     if (err) throw err;
-    books = results;
+    console.log("model.getAllBooks() - setting response");
+    res.json(books);
   });
-  return books;
 }
 
-function addBook(book) {
-  books[book.isbn] = book;
+function addBook(res, book) {
+  collection.insertOne(book, (err, result) => {
+    if (err) throw err;
+    console.log("1 document inserted: " + JSON.stringify(book));
+    res.send("Book added");
+  });
 }
 
-function updateBook(book) {
-  if (!(isbn in books)) {
-    throw new BookError("Unknown book with ISBN " + isbn);
-  }
-  books[book.isbn] = book;
+function updateBook(res, book) {
+  const query = { isbn: book.isbn };
+  const newValues = {$set: book};
+  collection.updateOne(query, newValues, (err, result) => {
+    if (err) throw err;
+    console.log("1 document updated: " + JSON.stringify(book));
+    res.send("Book updated");
+  });
 }
 
-function deleteBook(isbn) {
-  if (!(isbn in books)) {
-    throw new BookError("Unknown book with ISBN " + isbn);
-  }
-  delete books[isbn];
+function deleteBook(res, isbn) {
+  // Note need to parseInt as isbn is a number
+  const query = { isbn: parseInt(isbn) };
+  console.log("Deleting ", query);
+  collection.deleteOne(query, (err, obj) => {
+    if (err) throw err;
+    console.log("1 document deleted: " + isbn);
+    res.send("Book deleted");
+  });
 }
 
 // Export functions from Module
-module.exports = { getAllBooks, addBook, updateBook, deleteBook, BookError, setupMongo };
+module.exports = {
+  getAllBooks,
+  addBook,
+  updateBook,
+  deleteBook,
+  BookError,
+  setupMongo
+};
