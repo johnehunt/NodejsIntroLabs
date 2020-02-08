391460e2ab23f1d587adf0e6c098bb7892c9efc6
John Hunt <jeh@midmarsh.co.uk>
2020-02-08 16:55:22 +0000

    Added comments

--- lab1-hellobookshop/server.js
+++ lab1-hellobookshop/server.js
@@ -2,12 +2,15 @@ console.log("Starting Bookshop HTTP Server");
 // Load required http module
 const http = require("http");
 
+// Create the simple web application server
 const server = http.createServer(function(req, res) {
   res.write("<h1>Hello Bookshop World!</h1><p>Welcome to the Bookshop Everyone.</p>"); 
   res.end(); 
 });
 
+// Set the server to listen to port 8080
 console.log("Bookshop Listening on port 8080");
 server.listen(8080);
 
+// Notify ready to start operations
 console.log("Started - use http://localhost:8080/");
