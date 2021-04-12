fcd93087e48445fa59e9caac37d61bb9a8427901
John Hunt <john.hunt10@gmail.com>
2021-04-12 15:04:40 +0100

    Minor tidying

--- lab2-multiple-urls/server.js
+++ lab2-multiple-urls/server.js
@@ -1,6 +1,22 @@
 console.log("Starting Bookshop HTTP Server");
 // Load required http module
 const http = require("http");
+// Load commander module
+const commander = require('commander');
+
+// Check commander for actions
+commander
+  .option('-d, --debug', 'output extra debugging')
+  .option('-l, --long', 'provide long form information')
+  .option('-p, --printer <message>', 'pretty print a message', 'hello');
+
+commander.parse(process.argv);
+
+const options = commander.opts();
+
+if (options.debug) console.log('debug mode turned on');
+if (options.long) console.log('This provides long form information');
+if (options.printer) console.log(`printer ${options.printer}`);
 
 // Create a server that will respond to different URLs
 const server = http.createServer(function(req, res) {
@@ -25,5 +41,5 @@ const server = http.createServer(function(req, res) {
 console.log("Bookshop Server Listening on port 8080");
 server.listen(8080);
 
-console.log("Booksup Started");
+console.log("Bookshop Server Started");
 console.log("Try http://localhost:8080/");
