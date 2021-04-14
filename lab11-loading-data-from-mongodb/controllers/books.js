13df6ada3d8f3afa1c5af812943a66feadaeb5c4
John Hunt <john.hunt10@gmail.com>
2021-04-14 16:29:55 +0100

    layout updated

--- lab11-loading-data-from-mongodb/controllers/books.js
+++ lab11-loading-data-from-mongodb/controllers/books.js
@@ -8,34 +8,43 @@ function setupMongo() {
 // Defines Controller functions
 function getAllBooks(req, res) {
   console.log("routes.getAllBooks()");
-  Books.getAllBooks().then((books) => {
-    res.json(books);
-  }).catch((error) => {
-    console.log(error);
-    res.status(400).send();
-  });
+  Books
+    .getAllBooks()
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send();
+    });
 }
 
 function postBook(req, res) {
   console.log("postBook", req.body);
   const book = req.body;
-  Books.addBook(book).then(() => {
-    res.status(201).send();
-  }).catch((error) => {
-    console.log(error);
-    res.status(400).send();
-  });
+  Books
+    .addBook(book)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send();
+    });
 }
 
 function updateBook(req, res) {
   console.log("updateBook", req.body);
   const book = req.body;
-  Books.updateBook(book).then(() => {
-    res.status(202).send();
-  }).catch((error) => {
-    console.log(error);
-    res.status(400).send();
-  });
+  Books
+    .updateBook(book)
+    .then(() => {
+      res.status(202).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send();
+    });
 }
 
 function getBook(req, res) {
@@ -43,29 +52,40 @@ function getBook(req, res) {
     isbn: req.params.isbn,
     title: "Node.js Uncovered",
     author: "John Hunt",
-    price: 10.99
+    price: 10.99,
   });
 }
 
 function deleteBook(req, res) {
   const isbn = req.params.isbn;
-  Books.deleteBook(isbn).then(() => {
-    res.status(202).send();
-  }).catch((error) => {
-    console.log(error);
-    res.status(400).send();
-  });
+  Books
+    .deleteBook(isbn)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send();
+    });
 }
 
 function getData(req, rea) {
   const readStream = createReadStream("./data.txt");
-  readStream.on("data", data => {
+  readStream.on("data", (data) => {
     res.write(data);
   });
-  readStream.on("end", data => {
+  readStream.on("end", (data) => {
     res.status(200).send();
   });
 }
 
 // Now export functions from module
-module.exports = { getAllBooks, getBook, postBook, updateBook, deleteBook, getData, setupMongo };
+module.exports = {
+  getAllBooks,
+  getBook,
+  postBook,
+  updateBook,
+  deleteBook,
+  getData,
+  setupMongo,
+};
