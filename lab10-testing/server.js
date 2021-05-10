3d72923cf06ef3ddaf929d0f9c8e5d974fe6733f
John Hunt <john.hunt10@gmail.com>
2021-05-10 15:56:21 +0100

    Removed unused require

--- lab10-testing/server.js
+++ lab10-testing/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// To allow body to be parsed
-const bodyParser = require("body-parser");
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
