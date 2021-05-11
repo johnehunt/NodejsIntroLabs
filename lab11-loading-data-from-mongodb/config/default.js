6f035b265973569026311359afe5adb84550e407
John Hunt <john.hunt10@gmail.com>
2021-05-11 11:56:57 +0100

    Made config use dotenv

--- lab11-loading-data-from-mongodb/config/default.js
+++ lab11-loading-data-from-mongodb/config/default.js
@@ -1,4 +1,11 @@
+// Load and configure dotenv
+const dotenv = require("dotenv");
+const environment = dotenv.config();
+if (environment.error) {
+  throw environment.error;
+}
+
 // Set the port
-const port = 8080;
+const port = process.env.PORT;
 
 module.exports = {port}
